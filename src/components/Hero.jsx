36c64298d6d5b26e4646b05ee57f1c3a1cb2f581
abdulraheem5335 <abdurraheem5335@gmail.com>
2025-08-28 20:34:17 +0500

    Added img in Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -84,7 +84,7 @@ const Hero = () => {
     <section ref={heroRef} className="hero">
       <div className="hero-content">
         <div ref={logoRef} className="hero-logo">
-          <img src="/teacher-maddie-logo.png" alt="Teacher Maddie" />
+          <img src="../../public/logo.png" alt="Teacher Maddie" />
         </div>
         
         <h1 ref={titleRef} className="hero-title"></h1>
